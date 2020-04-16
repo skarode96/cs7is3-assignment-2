b43379d4c8f5cef8ef3568abb5f931fdf6affe81
skarode96 <skarode96@gmail.com>
2020-04-16 13:01:37 +0100

    updating instructions

--- compile.sh
+++ compile.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-
 mvn clean package -DskipTests
 mv ./target/newssearch-1.0.jar .
--- run.sh
+++ run.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+sh compile.sh
+
 export SVR_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+rm -rf Index
 
 JAVA_MAIN='ie.tcd.newssearch.App'
 JAVA_TUNE='-server -Xmx4g'
 
 java ${JAVA_TUNE} -cp .:${SVR_HOME}/newssearch-1.0.jar ${JAVA_MAIN}
+
+sh run_trec_eval.sh
